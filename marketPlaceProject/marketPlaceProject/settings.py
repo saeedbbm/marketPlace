b0e51c3f54f1e39b7bf77c3c680f15e00f1fe2cb
saeed <saeed.babamohamadi@gmail.com>
2021-12-12 08:58:54 -0800

    setup modifications

--- marketPlaceProject/marketPlaceProject/settings.py
+++ marketPlaceProject/marketPlaceProject/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # name of the apps should be added here
+    #'rest_framework',
+    'shopApp',
 ]
 
 MIDDLEWARE = [
---
+++ marketPlaceProject/shopApp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "shopApp"
+
+urlpatterns = [
+    # path('', views.index, name='index'), # for Function
+    # path('', views.Index.as_view(), name='index'), # for class
+]
