8bdd84230c93206934df8b58844621aff6212d67
Mohsen Bagheri <mohsenbaqeri10@gamil.com>
2021-12-20 09:28:24 +0330

    feat: users app has been added.

--- marketPlaceProject/marketPlaceProject/urls.py
+++ marketPlaceProject/marketPlaceProject/urls.py
@@ -20,5 +20,6 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shopApp/', include('shopApp.urls'))
+    path('shopApp/', include('shopApp.urls')),
+    path('users/', include('users.urls'))
 ]
